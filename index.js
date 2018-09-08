5714c98f544201be556cb64e80c8f8e25b192edf
Keshav Garg <keshav_garg@outlook.com>
2018-09-08 21:08:53 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -4,14 +4,17 @@
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var data = this.responseText;
+            data = JSON.parse(data);
+            
             data.forEach(function(article){
+
                 var child = document.createElement('div');
                 
                 var el = '<div class="card">' + 
-                    '<h2>' + article['name'] + '</h2>' +
-                    '<h5>Published ' + article['date'] + '</h5>' +
-                    '<p>' + article['snippet'] + '</p>' +
-                    '<a target="_blank" href=' + article['link'] + '>Read here</a>' +
+                    '<h2>' + article['name']['S'] + '</h2>' +
+                    '<h5>Published ' + article['date']['S'] + '</h5>' +
+                    '<p>' + article['snippet']['S'] + '</p>' +
+                    '<a target="_blank" href=' + article['link']['S'] + '>Read here</a>' +
                 '</div>'
 
                 child.innerHTML = el;
@@ -22,5 +25,5 @@
     };
     xhttp.open("GET", "https://jv1drclnm5.execute-api.us-east-2.amazonaws.com/beta/");
     xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send(JSON.stringify(article));
+    xhttp.send();
 })()
